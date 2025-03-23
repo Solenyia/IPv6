d8a9e669e418e1abe9fe7fd725d6b7ae0b3711e1
Solenyia <martin.hvozda1@gmail.com>
2025-03-23 13:57:21 +0100

    paramEdit

--- epy_block_0_n8i6u6ac.py
+++ epy_block_0_n8i6u6ac.py
@@ -13,7 +13,7 @@ from gnuradio import gr
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
     """Embedded Python Block example - a simple multiply const"""
 
-    def __init__(self, example_param=1.0):  # only default arguments here
+    def __init__(self, additionFlag=1.0):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
@@ -23,9 +23,9 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
         )
         # if an attribute with the same name as a parameter is found,
         # a callback is registered (properties work, too).
-        self.example_param = example_param
+        self.additionFlag = additionFlag
 
     def work(self, input_items, output_items):
         """example: multiply with constant"""
-        output_items[0][:] = input_items[0] * self.example_param
+        output_items[0][:] = input_items[0] * self.additionFlag
         return len(output_items[0])
