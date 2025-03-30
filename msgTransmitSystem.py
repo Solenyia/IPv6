0dc62a57a4db14b1413ce9ae03ee5216b3c69350
Solenyia <martin.hvozda1@gmail.com>
2025-03-30 17:40:51 +0200

    selecting port depending on boolean input

--- msgTransmitSystem.py
+++ msgTransmitSystem.py
@@ -13,19 +13,18 @@ from gnuradio import gr
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
     """Embedded Python Block example - a simple multiply const"""
 
-    def __init__(self, example_param=1.0):  # only default arguments here
+    def __init__(self):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
-            name='Embedded Python Block',   # will show up in GRC
-            in_sig=[np.complex64],
+            name='Multiplexer',   # will show up in GRC
+            in_sig=[np.complex64, np.complex64],
             out_sig=[np.complex64]
         )
         # if an attribute with the same name as a parameter is found,
         # a callback is registered (properties work, too).
-        self.example_param = example_param
 
     def work(self, input_items, output_items):
         """example: multiply with constant"""
-        output_items[0][:] = input_items[0] * self.example_param
+        output_items[0][:] = input_items[0]
         return len(output_items[0])
